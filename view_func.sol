9e818e3192e4fff237d57e8ea930a01f580b5044
Keerthana c <keerthanagitify@gmail.com>
2026-03-02 10:12:40 +0530

    Update view_func.sol

--- view_func.sol
+++ view_func.sol
@@ -1,9 +1,9 @@
 //SPDX-Licenses-Identifier:MIT
 pragma solidity^0.8.0;
-contract PureView{
-    uint stateVariable=5;
-
-    function pureFunc(uint a, uint b)public pure returns(uint){
-        return a + b;
+contract ViewExample{
+    uint public balance=100;
+    function changeBalance()public view returns(uint){
+        //balance=200;
+        return balance;
     }
 }
